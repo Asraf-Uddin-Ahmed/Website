9ae6101636931d4777242a450f30ca6a0e6addb5
ratul <13ratul@gmail.com>
2015-02-27 02:14:04 +0600

    SettingsRepository added.

---
+++ Website.Foundation/Repositories/SettingsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Website.Foundation.Aggregates;
+using Foundation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Website.Foundation.Repositories
+{
+    public class SettingsRepository : BaseEfRepository<Settings>, ISettingsRepository
+    {
+        private TableContext _context;
+        public SettingsRepository()
+        {
+            _context = new TableContext();
+        }
+    }
+}
--- Website.Web/Controllers/HomeController.cs
+++ Website.Web/Controllers/HomeController.cs
@@ -13,31 +13,40 @@ namespace Website.Web.Controllers
     {
         public ActionResult Index()
         {
-            Guid UserID = Guid.NewGuid();
-            UserRepository ur = new UserRepository();
-            ur.Add(new User()
-                {
-                    CreationTime = DateTime.UtcNow,
-                    EmailAddress = "test@test.com",
-                    ID = UserID,
-                    LastLogin = null,
-                    LastWrongPasswordAttempt = null,
-                    Password = "test",
-                    Status = UserStatus.Active,
-                    TypeOfUser = UserType.Admin,
-                    UpdateTime = DateTime.UtcNow,
-                    UserName = "test",
-                    WrongPasswordAttempt = 0
-                });
-            ICollection<User> l = ur.GetAll();
-            UserVerificationRepository uvr = new UserVerificationRepository();
-            uvr.Add(new UserVerification()
-                {
-                    CreationTime = DateTime.UtcNow,
-                    ID = Guid.NewGuid(),
-                    UserID = UserID,
-                    VerificationCode = Guid.NewGuid().ToString()
-                });
+            //Guid UserID = Guid.NewGuid();
+            //UserRepository ur = new UserRepository();
+            //ur.Add(new User()
+            //    {
+            //        CreationTime = DateTime.UtcNow,
+            //        EmailAddress = "test@test.com",
+            //        ID = UserID,
+            //        LastLogin = null,
+            //        LastWrongPasswordAttempt = null,
+            //        Password = "test",
+            //        Status = UserStatus.Active,
+            //        TypeOfUser = UserType.Admin,
+            //        UpdateTime = DateTime.UtcNow,
+            //        UserName = "test",
+            //        WrongPasswordAttempt = 0
+            //    });
+            //ICollection<User> l = ur.GetAll();
+            //UserVerificationRepository uvr = new UserVerificationRepository();
+            //uvr.Add(new UserVerification()
+            //    {
+            //        CreationTime = DateTime.UtcNow,
+            //        ID = Guid.NewGuid(),
+            //        UserID = UserID,
+            //        VerificationCode = Guid.NewGuid().ToString()
+            //    });
+            SettingsRepository sr = new SettingsRepository();
+            sr.Add(new Settings() 
+            { 
+                DisplayName = "dis name",
+                ID = Guid.NewGuid(),
+                Name = "main name",
+                Type = SettingsType.String,
+                Value = "hello"
+            });
             return View();
         }
 
