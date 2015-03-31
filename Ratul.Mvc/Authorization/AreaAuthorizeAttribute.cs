9f46485bcf6a4a99fd9b61cd3880d1292f505f85
ratul <ratul@proggasoft.com>
2015-03-31 17:04:26 +0600

    code re-factored.

--- Ratul.Mvc/Authorization/AreaAuthorizeAttribute.cs
+++ Ratul.Mvc/Authorization/AreaAuthorizeAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Ratul.Mvc
+namespace Ratul.Mvc.Authorization
 {
     public class AreaAuthorizeAttribute : AuthorizeAttribute
     {
