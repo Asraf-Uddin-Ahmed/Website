bd957daabd45ef36bd6acbcdd8c4de25dc282761
ratul <13ratul@gmail.com>
2015-04-12 12:47:14 +0600

    HtmlHelper added for active link of header menu.

---
+++ Ratul.Mvc/CustomHtmlHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Ratul.Mvc
+{
+    public static class CustomHtmlHelper
+    {
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, 
+            string linkText, string actionName, string controllerName, string cssClassName)
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+            if (actionName == currentAction && controllerName == currentController)
+            {
+                return htmlHelper.ActionLink(
+                    linkText,
+                    actionName,
+                    controllerName,
+                    null,
+                    new
+                    {
+                        @class = cssClassName
+                    });
+            }
+            return htmlHelper.ActionLink(linkText, actionName, controllerName);
+        }
+    }
+}
