9956a6b700e18946d15d6e1e7299f81065772d67
ratul <13ratul@gmail.com>
2016-06-03 15:46:50 +0600

    Bind all classes with default interfaces by Ninject

--- Website.Web/Codes/NinjectWebModule.cs
+++ Website.Web/Codes/NinjectWebModule.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Ninject.Modules;
-using System.Web.Mvc;
-using System.Web.Security;
-using System.Web;
-using System.Web.Hosting;
-using System.Configuration;
-using Ratul.Utility;
-using Website.Web.Codes.Persistence.Services;
-using Website.Web.Codes.Core.Services;
-using Website.Foundation.Persistence;
+﻿using Ninject.Modules;
 using Ninject.Web.Common;
+using Ratul.Utility;
+using Website.Foundation.Persistence;
 
 namespace Website.Web.Codes
 {
@@ -18,18 +9,8 @@ namespace Website.Web.Codes
     {
         public override void Load()
         {
-            /*
-             * MISCELLANEOUS
-             * */
             Bind<RegexUtility>().ToSelf();
             Bind<TableContext>().ToSelf().InRequestScope();
-
-            /*
-             * SERVICE
-             * */
-            Bind<IValidationMessageService>().To<ValidationMessageService>();
-            Bind<IUrlMakerService>().To<UrlMakerService>();
-            
         }
     }
 }
