bb2101c2f97c31a5d1cf57781191c5c6b478b5c8
ratul <13ratul@gmail.com>
2016-06-13 16:12:29 +0600

    ResponseCollectionModel added

---
+++ Website.WebApi/Models/Response/ResponseCollectionModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Website.Foundation.Core.Aggregates;
+using Website.Foundation.Core.SearchData;
+
+namespace Website.WebApi.Models.Response
+{
+    public class ResponseCollectionModel<TEntity, TModel> 
+        where TEntity : Entity
+        where TModel : ResponseModel
+    {
+        public IEnumerable<TModel> Items { get; set; }
+        public Pagination Pagination { get; set; }
+        public SortBy<TEntity> SortBy { get; set; }
+
+
+        public ResponseCollectionModel()
+        {
+            this.Pagination = new Pagination();
+            this.SortBy = new SortBy<TEntity>();
+        }
+    }
+}
