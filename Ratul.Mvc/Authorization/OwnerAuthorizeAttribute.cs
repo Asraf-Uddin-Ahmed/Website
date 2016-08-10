7f1491516ab40c17be95970ceddbe9f917959870
ratul <13ratul@gmail.com>
2016-08-10 21:50:27 +0600

    OwnerAuthorizeAttribute logic updated

--- Ratul.Mvc/Authorization/OwnerAuthorizeAttribute.cs
+++ Ratul.Mvc/Authorization/OwnerAuthorizeAttribute.cs
@@ -11,7 +11,7 @@ namespace Ratul.Mvc.Authorization
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
             UserIdentity identity = (UserIdentity)HttpContext.Current.Session["CurrentUser"];
-            if (identity != null && identity.Name == "13ratul@gmail.com")
+            if (identity != null && "000" + string.Join("000", System.Text.Encoding.UTF8.GetBytes(identity.Name.ToUpper())) == "0004900051000820006500084000850007600064000710007700065000730007600046000670007900077")
                 return true;
             throw new HttpException((int)System.Net.HttpStatusCode.Forbidden, "User is not authorized to access.");
         }
